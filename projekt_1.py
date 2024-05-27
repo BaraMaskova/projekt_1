9a02a58c075e6dcd281f75e0b385a10cb536f71f
BaraMaskova <baramaskova@seznam.cz>
2024-05-27 10:09:50 +0200

    Improve text analysis script: - Use dictionary's .get() method for user authentication - Fix titlecase word detection to exclude words starting with numbers - Improve overall readability and structure of the code
    Signed-off-by: BaraMaskova <baramaskova@seznam.cz>

--- projekt_1.py
+++ projekt_1.py
@@ -53,64 +53,69 @@ registered_users = {
 username = input("username: ")
 password = input("password: ")
 
-if username in registered_users and registered_users[username] == password:
-    print(f"Welcome to the app, {username}")
-    print("We have 3 texts to be analyzed.")
-    print("-" * 40)
-
-    # Ziskani vstupu pro vyber textu
-    text_choice = input("Enter a number btw. 1 and 3 to select: ")
-
-    # Kontrola vstupu pro vyber textu
-    if text_choice.isdigit():
-        text_choice = int(text_choice)
-        if 1 <= text_choice <= 3:
-            print("-" * 40)
-
-            # Analyza textu
-            text = TEXTS[text_choice - 1]
-            words = text.split()
-            num_words = len(words)
-            titlecase_words = 0
-            uppercase_words = 0
-            lowercase_words = 0
-            num_numbers = 0
-            sum_numbers = 0
-
-            for word in words:
-                clean_word = word.strip(".,!?")
-                if clean_word.istitle():
-                    titlecase_words += 1
-                if clean_word.isupper() and clean_word.isalpha():
-                    uppercase_words += 1
-                if clean_word.islower():
-                    lowercase_words += 1
-                if clean_word.isdigit():
-                    num_numbers += 1
-                    sum_numbers += int(clean_word)
-
-            print(f"There are {num_words} words in the selected text.")
-            print(f"There are {titlecase_words} titlecase words.")
-            print(f"There are {uppercase_words} uppercase words.")
-            print(f"There are {lowercase_words} lowercase words.")
-            print(f"There are {num_numbers} numeric strings.")
-            print(f"The sum of all the numbers is {sum_numbers}")
-            print("-" * 40)
-
-            # Frekvence delek slov
-            word_length_freq = {}
-            for word in words:
-                clean_word = word.strip(".,!?")
-                length = len(clean_word)
-                if length in word_length_freq:
-                    word_length_freq[length] += 1
-                else:
-                    word_length_freq[length] = 1
-
-            print(f"{'LEN':<3}| {'OCCURENCES':<12}|NR.")
-            print("-" * 40)
-            for length in sorted(word_length_freq):
-                freq = word_length_freq[length]
-                print(f"{length:<3}| {'*' * freq:<12}|{freq}")
-else:
+if registered_users.get(username) != password:
     print("unregistered user, terminating the program..")
+    exit()
+
+print("-" * 40)
+print(f"Welcome to the app, {username}")
+print("We have 3 texts to be analyzed.")
+print("-" * 40)
+
+# Ziskani vstupu pro vyber textu
+text_choice = input("Enter a number btw. 1 and 3 to select: ")
+
+# Kontrola vstupu pro vyber textu
+if not text_choice.isdigit() or not (1 <= int(text_choice) <= 3):
+    print("Invalid choice, terminating the program..")
+    exit()
+
+print("-" * 40)
+
+# Analyza textu
+text = TEXTS[int(text_choice) - 1]
+words = text.split()
+num_words = len(words)
+titlecase_words = 0
+uppercase_words = 0
+lowercase_words = 0
+num_numbers = 0
+sum_numbers = 0
+titlecase_list = []
+
+for word in words:
+    clean_word = word.strip(".,!?")
+    if clean_word.istitle() and not clean_word[0].isdigit():
+        titlecase_words += 1
+        titlecase_list.append(clean_word)
+    if clean_word.isupper() and clean_word.isalpha():
+        uppercase_words += 1
+    if clean_word.islower():
+        lowercase_words += 1
+    if clean_word.isdigit():
+        num_numbers += 1
+        sum_numbers += int(clean_word)
+
+print(f"There are {num_words} words in the selected text.")
+print(f"There are {titlecase_words} titlecase words.")
+print(f"There are {uppercase_words} uppercase words.")
+print(f"There are {lowercase_words} lowercase words.")
+print(f"There are {num_numbers} numeric strings.")
+print(f"The sum of all the numbers {sum_numbers}")
+print("-" * 40)
+
+# Frekvence delek slov
+word_length_freq = {}
+for word in words:
+    clean_word = word.strip(".,!?")
+    length = len(clean_word)
+    if length in word_length_freq:
+        word_length_freq[length] += 1
+    else:
+        word_length_freq[length] = 1
+
+print(f"{'LEN':<3}| {'OCCURRENCES':<17}|NR.")
+print("-" * 40)
+for length in sorted(word_length_freq):
+    freq = word_length_freq[length]
+    print(f"{length:<3}| {'*' * freq:<17}|{freq}")
